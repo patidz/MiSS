76fd86dd633e1619ecfe1653bc8dc4a62103b15d
patidz <pdzienisik@gmail.com>
2016-12-02 19:11:22 +0100

    Update HWIND.java

--- HWIND.java
+++ HWIND.java
@@ -6,14 +6,14 @@ package MiSS;
 public class HWIND extends AbstractTreeModel{
     private double ro;
     private double k;
-    private double g;
-    private int spacing;
+    private double g;   // 9.81 m/s2
+    private int spacing;    // odleglosc miedzy drzewami
     private int gapSize;
     private double meanTreeHeight;
 
     /**
      *
-     * @param treeHeights - wysokosci drzew w lesie (macierz X
+     * @param treeHeights - wysokosci drzew w lesie (macierz X)
      */
     void initialize(double[] treeHeights) {}
 
@@ -39,5 +39,51 @@ public class HWIND extends AbstractTreeModel{
     double calculateDistanceFromForestEdge(int treeI, int treeJ){
         return 0;
     }
+    /**
+     * pole powierzchni korony na danym odcinku i
+     */
+    double triangleSectorArea(Tree tree, int i){
+        double xleft_base=0,xleft_top=0,xright_base=0,xright_top=0,xmiddle=0,a=0,b=0,c=0,h1=0,h2=0,area=0;
+        double height = tree.Crown_depth;
+        double width = tree.Crown_width;
+        double dx = width/height;
+        double xleft = 0;
+        double xright = width;
 
+        xmiddle = width/2;
+        if (i < Math.floor(height/2)){
+            xleft_top = xmiddle - i*dx;
+            xright_top = xmiddle + i*dx;
+
+            xleft_base = xmiddle - (i+1)*dx;
+            xright_base = xmiddle + (i+1)*dx;
+        } else if (i >= Math.ceil(height/2)){
+            xleft_top = xleft + (i - height/2)*dx;
+            xright_top = xright - (i - height/2)*dx;
+
+            xleft_base = xleft + (i + 1 - height/2)*dx;
+            xright_base = xright - (i + 1 - height/2)*dx;
+        } else {
+            xleft_base = xmiddle - i*dx;
+            xright_base = xmiddle + i*dx;
+
+            xleft_top = xleft + (i + 1 - height/2)*dx;
+            xright_top = xright - (i + 1 - height/2)*dx;
+
+            a = xright_base - xleft_base;
+            b = width;
+            c = xright_top - xleft_top;
+
+            h1 = height/2 - i;
+            h2 = i + 1 - height/2;
+
+            area = ((a+b)/2)*h1 + ((b+c)/2)*h2;
+            return area;
+        }
+        a = xright_top - xleft_top;
+        b = xright_base - xleft_base;
+
+        area = (a+b)/2;
+        return area;
+    }
 }
