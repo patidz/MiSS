b0889df46a7603dc3cc7dc6dc40171733226b647
patidz <pdzienisik@gmail.com>
2016-12-02 19:14:56 +0100

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -6,18 +6,18 @@ package MiSS;
 public class Tree { // HWINDData
     protected int height;   // wysokosc drzewa
     protected int dbh;  // srednica drzewa na wysokosci piersi cz�owieka (1.3 m) [m]
-    protected int MOR;  // wspolczynnik pekania drewna [MPa]
-    protected int MOE;  // wspolczynnik elastycznosci [MPa]
-    protected int Cd;   // wspolczynnik tarcia
+    protected static double MOR = 39.1;  // wspolczynnik pekania drewna [MPa]
+    protected static double MOE = 7000;  // wspolczynnik elastycznosci [MPa]
+    protected static double Cd = 0.29;   // wspolczynnik tarcia
+    protected static double airDensity = 1.226;
     protected int Crown_mass;   // masa korony [kg]
     protected int Stem_mass;    // masa pnia [kg]
-    protected short f_RW;   // stosunek wagi gleby wokol korzeni do masy calego drzewa w [%]
+    protected static double f_RW = 0.3;   // stosunek wagi gleby wokol korzeni do masy calego drzewa w [%]
     protected int R_mass;   // masa korzeni [kg]
     protected int R_depth;  // glebokosc korzeni [m]
     protected int Crown_depth;  // wysokosc korony [m]
     protected int Crown_width;  // szerokosc korony [m]
-    protected Tree ScotsPines;
-    protected Tree NorwaySpruces;
+    //protected Tree NorwaySpruces;
 
     public Tree() {
 
