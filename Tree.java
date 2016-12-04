7039461dee2a02ad11c5c351519cff29f0545fc2
patidz <pdzienisik@gmail.com>
2016-12-04 20:57:54 +0100

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,25 +1,54 @@
 package MiSS;
 
 /**
- * Struktura drzew Tree
+ * Struktura drzew MiSS.miss.Tree
  */
-public class Tree { // HWINDData
-    protected int height;   // wysokosc drzewa
-    protected int dbh;  // srednica drzewa na wysokosci piersi cz�owieka (1.3 m) [m]
-    protected static double MOR = 39.1;  // wspolczynnik pekania drewna [MPa]
-    protected static double MOE = 7000;  // wspolczynnik elastycznosci [MPa]
-    protected static double Cd = 0.29;   // wspolczynnik tarcia
-    protected static double airDensity = 1.226;
-    protected int Crown_mass;   // masa korony [kg]
-    protected int Stem_mass;    // masa pnia [kg]
-    protected static double f_RW = 0.3;   // stosunek wagi gleby wokol korzeni do masy calego drzewa w [%]
-    protected int R_mass;   // masa korzeni [kg]
-    protected int R_depth;  // glebokosc korzeni [m]
-    protected int Crown_depth;  // wysokosc korony [m]
-    protected int Crown_width;  // szerokosc korony [m]
-    //protected Tree NorwaySpruces;
+public class Tree extends AbstractTreeModel{ // HWINDData
+    protected final double MOR = 39.1;  // wspolczynnik pekania drewna [MPa]
+    protected final double MOE = 7000;  // wspolczynnik elastycznosci [MPa]
+    protected final double Cd = 0.29;   // wspolczynnik tarcia
+    protected final double airDensity = 1.226;
+    protected final double f_RW = 0.3;   // stosunek wagi gleby wokol korzeni do masy calego drzewa w [%]
 
-    public Tree() {
+    protected boolean standing;
+    protected final double x;
+    protected final double y;
+    protected final int height;   // wysokosc drzewa
+    protected final int dbh = 1;  // srednica drzewa na wysokosci piersi człowieka (1.3 m) [m]
+    protected final int Crown_mass;   // masa korony [kg]
+    protected final int Stem_mass;    // masa pnia [kg]
+    protected final int R_mass;   // masa korzeni [kg]
+    protected final int R_depth;  // glebokosc korzeni [m]
+    protected final int Crown_depth;  // wysokosc korony [m]
+    protected final int Crown_width;  // szerokosc korony [m]
+    //protected miss.Tree ScotsPines;
+    // miss.Tree NorwaySpruces;
 
+    public Tree(int crown_depth, double x, double y, int height, int crown_mass, int stem_mass, int r_mass, int r_depth, int crown_width) {
+        Crown_depth = crown_depth;
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        Crown_mass = crown_mass;
+        Stem_mass = stem_mass;
+        R_mass = r_mass;
+        R_depth = r_depth;
+        Crown_width = crown_width;
+        standing = true;
     }
+
+    /**
+     *
+     * @param tree - struktura opisujaca drzewo
+     * @param speed - predkosc wiatru oddzialujaca na drzewo
+     * @param treeI - polozenie drzewa na macierzy X
+     * @param treeJ - polozenie drzewa na macierzy X
+     * @return 0 if drzewo stoi, 1 if drzewo zlamane, 2 if drzewo przewrocone
+     */
+    @Override
+
+    public double calculateTreeForce(Tree tree, double speed, int treeI, int treeJ){ //speedMatrix
+        return 0;
+    }
+
 }
